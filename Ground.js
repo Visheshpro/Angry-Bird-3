1067f6282c548e594d9cbd695900cf6dc0db0ecd
Vishesh Gupta <81924917+Visheshpro@users.noreply.github.com>
2021-05-19 20:40:38 +0530

    Add files via upload

---
+++ Ground.js
@@ -0,0 +1,10 @@
+class Ground extends BaseClass {
+    constructor(x,y,width,height) {
+     
+      super(x,y,width,height)
+
+      this.image = loadImage("sprites/ground.png")
+      this.body.isStatic = true
+    }
+
+  };
